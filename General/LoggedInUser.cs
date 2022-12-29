1beb534366909fc89c09255ab20922b66d4da135
hassanbassioun7 <97366708+hassanbassioun7@users.noreply.github.com>
2022-12-29 21:47:25 +0200

    Add files via upload

---
+++ General/LoggedInUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarAgency.General
+{
+    internal class LoggedInUser
+    {
+        public static string UserName { get; set; }
+        public static int RoleID { get; set; }
+    }
+}
